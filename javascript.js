280af006f63a28b0c36234170c1dc6a4490622a2
siemenlec <siemenlec@gmail.com>
2022-10-09 13:13:21 +0200

    backspace button javascript

--- javascript.js
+++ javascript.js
@@ -22,13 +22,7 @@ document
       numOnScreen += event.target.value;
       output.innerText = numOnScreen;
       console.log(equation);
-    } else if (
-      event.target.value === '-' &&
-      equation.slice(-1) !== '-' &&
-      equation.slice(-1) !== '+' &&
-      equation.slice(-1) !== '/' &&
-      equation.slice(-1) !== '*'
-    ) {
+    } else if (event.target.value === '-') {
       numOnScreen = '0';
       equation += event.target.value;
       output.innerText = numOnScreen;
@@ -58,13 +52,18 @@ document
       equation = '0';
       output.innerText = numOnScreen;
       console.info('CLEARED');
+    } else if (event.target.value === 'back') {
+      numOnScreen = numOnScreen.slice(0, -1);
+      console.log(numOnScreen);
+      output.innerText = numOnScreen;
+      equation = equation.slice(0, -1);
     } else if (event.target.value === '=') {
       try {
         const outcome = eval(equation);
         console.log(`Equals: ${outcome}`);
         output.innerText = outcome;
         equation = outcome.toString();
-        numOnScreen = '0';
+        numOnScreen = outcome.toString();
       } catch (error) {
         console.error(error);
         // Going to give error if an operator is pressed multiple times
