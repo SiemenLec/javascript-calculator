ebbeaa4af73c1b1c8b738d655d9870910249972c
Siemen Lecoutre <siemen.lecoutre@outlook.be>
2025-04-26 06:31:37 +0200

    remove duplicate '-' condition / removed console logs

--- javascript.js
+++ javascript.js
@@ -21,52 +21,37 @@ document
       equation += event.target.value;
       numOnScreen += event.target.value;
       output.innerText = numOnScreen;
-      console.log(equation);
     } else if (event.target.value === '-') {
       numOnScreen = '0';
       equation += event.target.value;
       output.innerText = numOnScreen;
-      console.log(equation);
     } else if (event.target.value === '+') {
       numOnScreen = '0';
       equation += event.target.value;
       output.innerText = numOnScreen;
-      console.log(equation);
     } else if (event.target.value === '/') {
       numOnScreen = '0';
       equation += event.target.value;
       output.innerText = numOnScreen;
-      console.log(equation);
     } else if (event.target.value === '*') {
       numOnScreen = '0';
       equation += event.target.value;
       output.innerText = numOnScreen;
-      console.log(equation);
-    } else if (event.target.value === '-') {
-      numOnScreen = '0';
-      equation += event.target.value;
-      output.innerText = numOnScreen;
-      console.log(equation);
     } else if (event.target.value === 'clear') {
       numOnScreen = '0';
       equation = '0';
       output.innerText = numOnScreen;
-      console.info('CLEARED');
     } else if (event.target.value === 'back') {
       numOnScreen = numOnScreen.slice(0, -1);
-      console.log(numOnScreen);
       output.innerText = numOnScreen;
       equation = equation.slice(0, -1);
     } else if (event.target.value === '=') {
       try {
         const outcome = eval(equation);
-        console.log(`Equals: ${outcome}`);
         output.innerText = outcome;
         equation = outcome.toString();
         numOnScreen = outcome.toString();
       } catch (error) {
-        console.error(error);
-        // Going to give error if an operator is pressed multiple times
         output.innerText = 'ERROR';
         equation = '0';
         numOnScreen = '0';
